779744347e61a12f3cea72e956f71ebf96964205
theNerd247 <noah.harvey247 gm>
2020-06-04 15:08:38 -0700

    finished initial tests

--- test/Arbitrary/Repline.hs
+++ test/Arbitrary/Repline.hs
@@ -1,57 +1,47 @@
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 
 module Arbitrary.Repline where
 
+import Data.Char (isLetter)
+import Data.Foldable (toList)
+import Data.Monoid (Alt(..), Ap(..), First(..))
+import Options.Applicative
 import Options.Repline
 import Test.QuickCheck
-import Options.Applicative
 
--- The goal here is to generate random parsers for various
--- needs. Here is a sketch of some of the properties we'll
--- need to test:
---
---
--- Ensure proper cmd names extraction
--- 1. A parser with no toplevel subparser produces an empty list of toplevel commands
--- 2. A parser with N commands under a single subparser produces N toplevel commands
--- 3. A parser with M subparsers produces sum(Ni, 0, M) toplevel commands
---
--- Ensure calling a optparse-applicative parser is the same as appending command name
--- 1. Given command names NS and an Identity parser (idPN :: OptParser (Maybe CmdName))
--- then forall N in NS: runParser p [N] == Just N
--- 2. Given a command name N and idPN (previous item): runParser p [N, "--help"] == Nothing
--- assuming optparse-applicative defines the "--help" flags as identical to
--- failing a parser
---    
+subParserTestParser :: [[CmdName]] -> ParserInfo CmdName
+subParserTestParser = emptyParser . getAlt . foldMap (Alt . subparser . foldMap mkCommand)
 
-emptyTestParser :: Gen (OptParser (Maybe ()))
-emptyTestParser = pure $ testParser $ pure ()
-
-singleSubTestParser :: Gen (OptParser (Maybe CmdName))
-singleSubTestParser = testParser . subparser <$> genCommand
-
-multiCmdSubTestParser :: Gen (OptParser (Maybe CmdName))
-multiCmdSubTestParser = testParser . subparser <$> (nMonoids genCommand)
-
-multiSubTestParser :: Gen (OptParser (Maybe CmdName))
-multiSubTestParser = fmap testParser $ resize 10 $ subparser <$> genCommand
-
-testParser :: Parser a ->  OptParser (Maybe a)
-testParser p = OptParser
+testOptParser :: ParserInfo a ->  OptParser (Maybe a)
+testOptParser p = OptParser
   { parserPrefs        = defaultPrefs
-  , parserInfo         = info p mempty
+  , parserInfo         = p
   , handleParserResult = getParseResult
   }
 
-nMonoids :: (Monoid m) => Gen m -> Gen m
-nMonoids g = mconcat <$> (listOf g)
+emptyParser :: Parser a -> ParserInfo a
+emptyParser = flip info mempty 
 
-genCommand :: Gen (Mod CommandFields CmdName)
-genCommand = mkCommand <$> arbitraryCmdName
+arbitrarySubParserCmdNames :: Gen [[CmdName]]
+arbitrarySubParserCmdNames = resize 10 $ listOf arbitraryCmdNames
+
+arbitraryCmdNames :: Gen [CmdName]
+arbitraryCmdNames = resize 10 $ listOf arbitraryCmdName
 
 arbitraryCmdName :: Gen CmdName
-arbitraryCmdName = resize 7 $ listOf arbitraryASCIIChar
+arbitraryCmdName = resize 7 $ listOf $ arbitrary `suchThat` isLetter
 
+-- Creates a command parser that always succeeds and returns its name. This is
+-- used to determine if the correct internal parser is called when commandline
+-- args that corresponds to the created parser
 mkCommand :: CmdName -> Mod CommandFields CmdName
 mkCommand cmdName = command cmdName $ info (pure $ cmdName) mempty
+
+withSelected :: (Foldable t) => t a -> Gen (t a, Maybe a)
+withSelected t = ((t,)) <$> (maybeChooseFirst t)
+
+maybeChooseFirst :: (Foldable t) => t a -> Gen (Maybe a)
+maybeChooseFirst = fmap getFirst . getAp . foldMap (\x -> Ap $ First <$> elements [Nothing, Just x])
--- test/Main.hs
+++ test/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
-import Test.Tasty
-import Test.Tasty.QuickCheck as QC
-
+import Arbitrary.Repline
+import Data.Functor.Compose (Compose(..))
+import Data.List (sort)
+import Options.Applicative
 import Options.Repline
-
+import Test.QuickCheck
+import Test.Tasty
+import qualified Test.Tasty.QuickCheck as QC
 
 main = defaultMain tests
 
@@ -13,5 +16,37 @@ tests = testGroup "Properties" [qcProperties]
 
 qcProperties :: TestTree
 qcProperties = testGroup "QuickCheck" 
-  [ 
+  [ collectCmdNamesTest
+  , runParserTest
   ]
+
+collectCmdNamesTest :: TestTree
+collectCmdNamesTest = QC.testProperty "Collected Command Names From Parser" collectCmdNamesProp
+
+runParserTest :: TestTree
+runParserTest = QC.testProperty "runParser gives back correct cmd" runParserProp
+
+collectCmdNamesProp :: Property
+collectCmdNamesProp = forAll arbitrarySubParserCmdNames $ \subParsersCmdNames ->
+    let collectedNames = sort $ collectTopLevelCmdNames $ subParserTestParser subParsersCmdNames
+        cmdNames = sort $ mconcat subParsersCmdNames
+    in 
+      counterexample ("Collected names: " <> (show collectedNames))
+    $ collectedNames == cmdNames
+
+-- Tests that calling the toplevel parser with one of the
+-- generated command names produces the same command name in the result. This ensures that
+-- the optparse-applicative parser will work with the repline parser after we've appended
+-- the command name to the list of arguments.
+runParserProp  :: Property
+runParserProp = forAll (arbitrarySubParserCmdNames >>= withSelected . Compose) $ 
+  \(subParsersCmdNames, selectedCmdName) ->
+  let 
+    parsedCmd = 
+      selectedCmdName 
+      >>= runParser 
+        (testOptParser . subParserTestParser . getCompose $ subParsersCmdNames) 
+        . pure
+  in 
+    counterexample ("Parser returned: " <> (show parsedCmd))
+  $ parsedCmd == selectedCmdName
